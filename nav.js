80aa70c17af938e5e78ddbf4229938cfdede91ee
jordanharmon13 <jordanharmon13@gmail.com>
2015-10-24 11:12:40 -0600

    jQuery code, new animations, and testing differnt projects from treehouse

---
+++ nav.js
@@ -0,0 +1,15 @@
+var nav = '<div>
+	<ul>
+		<li>JavaScript Objects</li>
+		<li><a href="cit261/transformations.html">Transformations</a></li>
+		<li><a href="cit261/transitions.html">Transitions</a></li>
+		<li><a href="cit261/animations.html">Animations</a></li>
+		<li><a href="cit261/team-material.html">Team Materials</a></li>
+		<li><a href="cit261/js-example.html">JavaScript</a></li>
+		<li><a href="cit261/ajax.html">AJAX</a></li>
+		<li><a href="local-storage/index.html">Local Storage</a></li>
+		<li><a href="jquery/index.html">jQuery</a></li>
+	</ul>
+</div>';
+
+document.write(nav);
